16458fb5b219f206b63d8a309ee931ac62463d59
zgaoliang520 <33594159+zgaoliang520@users.noreply.github.com>
2019-12-25 00:00:34 +0800

    Create jxydt.js

---
+++ jxydt.js
@@ -0,0 +1,21 @@
+/*
+# 驾照一点通vip
+QX1.0.0:
+^https:\/\/vipapi\.jxedt\.com\/vip\/check url script-response-body jianlou/jxydt.js
+Surge4.0:
+http-response ^https://vipapi.jxedt.com/vip/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Alex0510/surge/master/Script/jxydt.js,script-update-interval=0
+MITM = vipapi.jxedt.com
+*/
+
+var obj = JSON.parse($response.body);
+ obj={
+ "data": {
+  "is_vip": "1",
+  "vip_expired": "4092599349",
+  "is_vip_now": true
+ },
+ "error": false,
+ "status": 200
+};
+$done({body: JSON.stringify(obj)});
+//
